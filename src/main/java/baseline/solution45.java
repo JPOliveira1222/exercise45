279b408452574537298671c6e3deeeebd35d4ba5
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-16 20:08:17 -0400

    pseudocode for exercise 45

---
+++ src/main/java/baseline/solution45.java
@@ -0,0 +1,54 @@
+package baseline;
+
+public class solution45 {
+    /*
+    Create a class that finds word
+    create a method that throws an IOException
+
+    create a scanner
+    print "Enter the output file: "
+
+    create a string output that scans the next line
+    create a readfile object
+
+    read the file
+    get the list that contains the data of file and put it in an array list
+
+    create an object that writes a file
+    write the file
+    print the number of modifications on console
+    print "The number of modifications are : " +.getmodification
+
+
+
+    create a class that reads the file
+    create a private an arraylist
+    create a private scanner
+
+    constructor
+    pass the path to the file
+    create a new scanner
+
+    use try and finally for exceptions
+
+    read the file line by line and store it into a list
+
+    close reader object
+    return the list
+
+
+    create a class to write into a file
+    create instance variables for a file writer, count modification, and arraylist of strings
+
+    initialize variables of a new filewriter
+
+    write into file
+    using a for loop for the size of the list
+    get the list
+    count modifications using while loop
+    close the writer object
+
+    return the count of modifications
+
+     */
+}
